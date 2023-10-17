b6255605e236688fb40d265cf09dd576ce8d2e15
Elijah <eachala5@gmail.com>
2023-10-17 12:27:42 -0700

    t1

--- nonprintable.c
+++ nonprintable.c
@@ -7,11 +7,12 @@
  */
 int nonprintable(va_list *ptr, char **pc)
 {
-int i, j;
+int i, j, n;
 int len;
 char *hexval;
 char *str = va_arg(*ptr, char *);
 len = _strlen(str);
+n = 0;
 for (i = 0; i < len; ++i)
 {
 if (((str[i] > 0) && (str[i] < 32)) || (str[i] >= 127))
@@ -19,23 +20,27 @@ if (((str[i] > 0) && (str[i] < 32)) || (str[i] >= 127))
 hexval = int_hexc(str[i]);
 putbuff('\\', pc);
 putbuff('x', pc);
+n = 2;
 if (_strlen(hexval) > 1)
 {
 for (j = 0; j < _strlen(hexval); ++j)
 {
 putbuff(hexval[j], pc);
+++n;
 }
 }
 else
 {
 putbuff('0', pc);
 putbuff(*hexval, pc);
+n += 2;
 }
 }
 else
 {
 putbuff(str[i], pc);
+++n;
 }
 }
-return (1);
+return (n);
 }
